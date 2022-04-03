7921cd3021a3566680eda0bdab18af44648f19c5
David <66866223+david-prv@users.noreply.github.com>
2022-04-03 17:14:57 +0200

    Create lac.sourcebook.js

---
+++ src/lac.sourcebook.js
@@ -0,0 +1,12 @@
+/*
+ * External source for keywords
+ */
+const KEYWORD_LIST = [
+  "agb",
+  "geschäftsbedingungen",
+  "tos",
+  "geschaeftsbedingungen",
+  "terms",
+  "terms of service",
+  "privacy"
+];
